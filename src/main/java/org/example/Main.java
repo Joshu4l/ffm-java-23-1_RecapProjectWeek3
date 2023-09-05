9d7cb00682d772b00bd8410dc110f0dab2fe5660
leonietschapalda <ltschapalda@googlemail.com>
2023-09-05 16:52:44 +0200

    Changes by joshuaalbert end of day

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /*
     Erstelle eine Klasse ProductRepo, die eine Liste von Product-Objekten enthält.
@@ -24,11 +22,19 @@ public class Main {
         Product my3rdProduct = new Product("P-0003","Triangel", "Kling", "Dreieck", 10);
 
         // Create an empty Array List ...
-        List<Product> productsToBeOrdered = new ArrayList<>();
+        // List<Product> productsToBeOrdered = new ArrayList<>();
+        Map<String, Integer> productsToBeOrdered = new HashMap<>();
+        productsToBeOrdered.put(my1stProduct.id(), 3);
+        productsToBeOrdered.put(my2ndProduct.id(), 2);
+        productsToBeOrdered.put(my3rdProduct.id(), 5);
+
+        System.out.println(productsToBeOrdered);
+
         // ... and add some products
         productsToBeOrdered.add(my1stProduct);
         productsToBeOrdered.add(my2ndProduct);
         // Now tell our ShopService to deal with our desired list of products (and under which reference he can save it)
+        //TODO:
         myShopService.placeNewOrder("O-0001", productsToBeOrdered);
 
     }
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,6 +3,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public record Order(String id, List<Product> productsToBeOrdered) {
+public record Order(String id, Map<String, Integer> productsToBeOrdered) {
 
 }
